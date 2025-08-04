fd2fb2a39a063e157343fb25c6dd68fdae1fede6
vikas566 <70830042+vikas566@users.noreply.github.com>
2025-08-04 16:26:19 +0530

    Add files via upload

---
+++ script (4).js
@@ -0,0 +1,8 @@
+<script>
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navbar = document.querySelector('.navbar');
+
+  menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+  });
+</script>
